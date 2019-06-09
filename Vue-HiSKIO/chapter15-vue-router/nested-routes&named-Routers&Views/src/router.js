f13f989756e7bdac1b5a7091b5e8491b487a839a
Orow Chou <jou00088@hotmail.com>
2019-06-09 14:00:13 +0800

    named routes & view finished

--- Vue-HiSKIO/chapter15-vue-router/nested-routes&named-Routers&Views/src/router.js
+++ Vue-HiSKIO/chapter15-vue-router/nested-routes&named-Routers&Views/src/router.js
@@ -22,11 +22,22 @@ export default new VueRouter({
           component: About,
           children: [
             { path: "", component: AboutAllOfUs },
-            { path: "us", component: AboutUs },
+            // 把下面這個組件AboutUs命名為Us，在template中的to的路徑改為{name: 'us'}，to改為:to動態綁定
+            { path: "us", name:'us', component: AboutUs },
             { path: "family", component: AboutFamily },
+            { path: "both", components: {
+              // a: AboutUs,
+              // b: AboutFamily,
+              default: AboutUs,
+              another: AboutFamily,
+            }},
           ],
         },
-        { path: "/products/:id?", component: Products },
+        { path: "/products/:id?",
+          // 具名路由
+          name: 'prods',
+          component: Products,
+        },
       ],
     },
   ],
