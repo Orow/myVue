64eb3ad98d4bec8f5e45c6687eff16a22a2322ae
Orow Chou <jou00088@hotmail.com>
2019-07-07 21:53:06 +0800

    state

--- Vue-HiSKIO/chapter16-vuex/basic_store_state/src/App.vue
+++ Vue-HiSKIO/chapter16-vuex/basic_store_state/src/App.vue
@@ -30,10 +30,28 @@ export default {
   // mapMutations
   // methods: mapMutations(['addCount']),
 
+  data(){
+    return {
+      localCount: 0,
+    };
+  },
+
   // mapState, mapMutations 結合ES6 Spread 語法來寫多個 computed 跟 methods
   computed: {
-   ... mapState(['count'])
+    ... mapState(['count']),
+    //  本身實例的computed
+    myCount(){
+      return this.localCount;
+    },
   },
+
+  // mapState 傳入陣列
+  // computed: mapState([
+  //   'count',
+  //   'lists',
+  //   'username'
+  // ]),
+
   methods: {
     ...mapMutations(['addCount'])
   },
@@ -42,30 +60,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- Vue-HiSKIO/chapter16-vuex/basic_store_state/src/store.js
+++ Vue-HiSKIO/chapter16-vuex/basic_store_state/src/store.js
@@ -4,15 +4,24 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+const store = new  Vuex.Store({
+    // 儲存狀態，會用到的先定義，是空的也沒關係
     state:{ 
         count: 0,
+        usernsme: '',
+        list: [],
     },
     mutations: {
         addCount(state){
             state.count += 1;
+
+            // 不存在的狀態屬性
+            // Vue.set(state, 'loading', false);
         },
     },
+    
+    // 以上 state
+    // 以下 mutations
 });
 
 export default store;
