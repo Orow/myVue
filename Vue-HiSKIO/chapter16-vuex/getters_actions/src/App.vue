3942b01bd4209c5044d4a3c5e4e0ce39d994b194
Orow Chou <jou00088@hotmail.com>
2019-07-09 00:38:30 +0800

    getters&actions done

--- Vue-HiSKIO/chapter16-vuex/getters_actions/src/App.vue
+++ Vue-HiSKIO/chapter16-vuex/getters_actions/src/App.vue
@@ -6,14 +6,48 @@
 </template>
 
 <script>
-import {mapState, mapMutations} from 'vuex';
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
 export default {
   computed : {
-    ...mapState(['count']),
+    ...mapState(['items']),
+
+    // 只有這個組件用到的話就寫在這 - filter 出還沒有 done 的 item 然後計算長度
+    itemsNotFinish(){
+      return this.items.filter(item => !item.done).length;
+    },
+    // 從 store 傳來，使用 mapGetters
+    ...mapGetteres(['itemNotFinish']),
   },
   methods: {
     ...mapMutations(['addCount']),
+    test(){
+      this.itemsWithID(2) // id=2 傳入
+    },
   },
+
+  // 以上 getters
+  // 以下 actions
+
+  mounted() {
+    this.$store.dispatch('fetchTodos');
+    // 傳入第二個參數當 payload
+    this.$store.dispatch('fetchTodos', {id:2});
+    // 也一樣可以傳入物件
+    this.$store.dispatch({
+      type: 'fetchTodos',
+      id: 2,
+    });
+
+    // 用 mapActions 後
+    this.fetchTodos({id:2});
+
+    // 回傳 promise 並 resolve 可以當 promise 用
+    this.fetchTodos({id:2})
+    .then(()=>{
+      
+    })
+  },
+  methods: mapActions(['fetchTodos']),
 
 };
 </script>
--- Vue-HiSKIO/chapter16-vuex/getters_actions/src/store.js
+++ Vue-HiSKIO/chapter16-vuex/getters_actions/src/store.js
@@ -1,24 +1,100 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const store = new  Vuex.Store({
-    // 儲存狀態，會用到的先定義，是空的也沒關係
-    state:{ 
-        count: 0,
-        usernsme: '',
-        list: [],
-    },
-    // 透過 mutations 改變 state
-    mutations: {
-        addCount(state){
-            state.count += 1;
+const url = "";
 
-            // 不存在的狀態屬性
-            // Vue.set(state, 'loading', false);
-        },
+const store = new Vuex.Store({
+  state: {
+    items: [],
+    todos: [],
+    loading: false
+  },
+  // getters 跟 mutations 很像，都是函式，需要傳入 state 參數
+  getters: {
+    itemsNotFinish(state) {
+      return state.items.filter(item => !item.done).length;
     },
+    // 另外一個 getters 要用到其他 getters
+    itemsFinish(state, getters) {
+      return state.items.length - getters.itemsNotFinish;
+    },
+    // 也可以回傳函式，在組件中就可以
+    itemsWithID(state) {
+      return id => {
+        return state.items.filter(item => item.id === id);
+      };
+    },
+    // 箭頭函式縮寫
+    itemsWithID: state => id => state.items.filter(item => item.id === id)
+  },
+  mutations: {
+    // actions 發出 mutations 用來變更 state 中的 todos
+    setTodos(state, todos) {
+      state.todos = todos;
+    },
+    setLoading(state, loading) {
+      state.loading = loading;
+    }
+  },
+  actions: {
+    //  context 包含 store 的一切，還有其他東西，在模組化會提到
+    // state, mutations, dispatch, commit都包含在 context 中
+    //   fetchTodos(context){
+    //       fetch(`${url}`/todos)
+    //         .then(res => res.json())
+    //         .then(todos => {
+    //             // fetch 後取得的 todos 要指定回去 state 中的 todos
+    //             // 透過發出 mutations 的方式
+    //             context.commit('setTodos', todos);
+    //         })
+    //   },
+
+    // 只用到 commit
+    //   fetchTodos({commit}, payload){
+    // dispatch
+    fetchTodos({ commit }, payload) {
+      commit("setLoading", true);
+
+      fetch(`${url}` / todos)
+        .then(res => res.json())
+        .then(todos => {
+          commit("setTodos", todos);
+          commit("setLoading", false);
+          this.dispatch("fetchUserInfo");
+        });
+    },
+    fetchUserInfo() {},
+
+    // actions 可以回傳 promise
+    // 最後 resolve 後在 mounted 呼叫的時候可以當 promise 用
+    fetchTodos({ commit }, payload) {
+      return new Promise(resolve => {
+        commit("setLoading", true);
+        fetch(`${url}` / todos)
+          .then(res => res.json())
+          .then(todos => {
+            commit("setTodos", todos);
+            commit("setLoading", false);
+            this.dispatch("fetchUserInfo");
+            resolve();
+          });
+      });
+    },
+    fetchTodos({ commit }, payload) {
+      commit("setLoading", true);
+      // fetch 已經是 promise 就可以直接 return ，不需要 return new Promise
+      return fetch(`${url}` / todos)
+        .then(res => res.json())
+        .then(todos => {
+          commit("setTodos", todos);
+          commit("setLoading", false);
+          this.dispatch("fetchUserInfo");
+          resolve();
+        });
+    }
+  }
 });
 
 export default store;
