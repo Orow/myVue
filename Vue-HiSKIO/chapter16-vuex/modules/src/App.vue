e1f72399b9140e4710e577907dd357bc16be4f18
Orow Chou <jou00088@hotmail.com>
2019-08-28 11:44:57 +0800

    vuex-modules - init

---
+++ Vue-HiSKIO/chapter16-vuex/modules/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <button @click="addCount">add-test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+  methods: {
+    addCount() {
+      this.$store.commit('addCount');
+    },
+  },
+}
+</script>
